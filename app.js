939e84244ee9f2df3d9142b658a45a769596da85
Karan <karan@karans-air.home>
2023-03-14 13:04:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/newapi').then(()=>{
+    console.log("Databse is connected");
+}).catch((err)=>{
+    console.log(err);
+});
+
+// @Schema
+
+const studentSchema = new mongoose.Schema({
+    name:String,
+    lastname:String,
+    rollNo: Number
+});
+ 
+//@model
+
+const Student = new mongoose.model('Student', studentSchema);
+
+//@product-creating-api
+
+app.post('/api/v1/student/new', async (req, res)=>{
+    const student =  await Student.create(req.body);
+    res.status(201).json({
+        sucess:true,
+        message:'Student is added',
+    });
+});
+
+app.get('/api/v1/student/get', async (req, res)=>{
+    const student = await Student.find();
+
+    res.status(200).json({
+        sucess:true,
+    });
+});
+
+
+app.put('/api/v1/student/update/:id', async (req, res)=>{
+    let student = await Student.findById(req.params.id);
+
+    student = await Student.findByIdAndUpdate(req.params.id,req.body,{
+
+        new:true,
+        useFindAndModify:false,
+        runValidators:true,
+    });
+
+    res.status(200).json({
+        sucess:true,
+        student,
+    })
+});
+
+
+app.delete('/api/v1/student/delete/:id',async (req, res)=>{
+    try{
+        let student = await Student.findByIdAndDelete(req.params.id);
+        res.status(201).json({
+            suceess:true,
+            message:'Product has been deleted sucessfully',
+        })
+    }
+    catch(err){
+        res.status(404).json({
+            sucess:false,
+            message:"page not found error"
+        })
+    }
+});
+
+app.listen(4000, ()=> console.log('Server is running on localhost:4000'))
---
+++ test.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/restapi',).then(()=>{
+    console.log("Data base is connected");
+}).catch((err)=>{
+    console.log(err);
+});
+
+// Schema for product -- @Start Point
+
+const productSchema = new mongoose.Schema({
+    name:String,
+    description:String,
+    price:Number
+});
+
+const Product = new mongoose.model("Product", productSchema);
+
+// ------- Schema has been Created -- @EndPoint...
+
+// @CreatePoduct
+app.post('/api/v1/product/new', async (req, res)=>{
+    const product = await Product.create(req.body)
+
+    res.status(201).json({
+        sucess:true,
+    });
+});
+  
+
+// @readProducts
+
+app.get('/api/v1/product',async (req, res)=>{
+    const products = await Product.find();
+
+    res.status(200).json({
+        sucess:true,
+        products,
+    })
+});
+
+
+// @productUpdate 
+
+app.put("/api/v1/product/:id", async (req, res)=>{
+    let product = await Product.findById(req.params.id);
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{new:true, useFindAndModify:false,runValidators:true,
+
+    })
+    res.status(200).json({
+        success:true,
+        product,
+    });
+})
+
+// @productDelete
+
+app.delete('/api/v1/product/:id', async (req, res)=>{
+    try{
+        const product = await Product.findByIdAndDelete(req.params.id);
+
+        if(!product)
+        req.json(404)
+        
+        res.json({
+            sucess:true,
+            message:"product is deleted sucessfully",
+        });
+
+    } catch(err){
+        res.json({sucess:false, message:"Page not found"});
+    }
+}) 
+
+app.listen(4000,()=> console.log('Server is working'))
+
+
